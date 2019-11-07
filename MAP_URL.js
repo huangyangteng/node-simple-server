7c0463ba2cdc4b626d50953c16ed19081d89375a
hyangteng <hyangteng@gmail.com>
2019-11-07 12:38:45 +0800

    ok

--- MAP_URL.js
+++ MAP_URL.js
@@ -16,6 +16,10 @@ const {administrator,task,error,role,province} =Controller
 const MAP_URL={//顺序很重要 遍历时是按照顺序进行的  /user /user/add 
     'default':Controller.default,
     '/api/administrator/login':administrator.login,
+    '/api/administrator/all':administrator.all,
+    '/api/administrator/add':administrator.add,
+    '/api/administrator/edit':administrator.edit,
+    '/api/administrator//batch/delete':administrator.delete,
     '/task/array':task.array,
     'error':error.random,
     '/api/role/all':role.all,
--- api/administrator.js
+++ api/administrator.js
@@ -1,20 +1,98 @@
 const Res=require('./resCommon')
-const administrator={
-    login(){
-        let data={
-               id:1,
-               name:'super admin',
-               phoneNumber:'15812233158',
-               email:'15812233158@ebupt.com',
-               password:'271c3f3c8aa14d9a82703b4f3a6cfcdb',
-               role:[],
-               permission:[],
-               createAdminId:-1,
-               createData:'20190808',
-               adminState:'00'
+const fs=require('fs')
+const tools=require('./tools')
 
+const ADMIN_DATA_PATCH='data/administrator.json'
+
+class Admin{
+    constructor(){
+        let adminList=tools.getJsonByFile(ADMIN_DATA_PATCH)
+        this.key=adminList.key
+        this.list=adminList.list
+        this.adminList=adminList
+    }
+    login(reqData){
+        if(!reqData.adminName || !reqData.adminPassword){
+            return Res.error('80003','参数填错了')
         }
-        return Res.success(data)
+        let {adminName}=reqData
+        let result=this.list.find(item=>item.name==adminName)
+        if(result){
+            return Res.success(result)
+        }else{
+            return Res.error('80004','该用户不存在')
+        }
+    }
+    all(){
+        return Res.success(this.list)
+    }
+    add(reqData){
+        console.log("TCL: Admin -> add -> reqData", reqData)
+        if(!reqData.name || ! reqData.phoneNumber || !reqData.email || !reqData.role){
+            return Res.error('80003','参数填错了')
+        }
+
+        let {name}=reqData
+        if(this.list.findIndex(item=>item.name==name) != -1){
+            return Res.error('80004','该用户名已被注册')
+        }
+
+        reqData.id=this.adminList.key++
+        this.list.push(reqData)
+        tools.writeJsonToFile(ADMIN_DATA_PATCH,this.adminList)
+
+        return Res.success({id:this.key})
+        
+    }
+    edit(){
+        
+    }
+    delete(){
+
+    }
+}
+
+
+const administrator={
+
+    login(req){
+        const {method} =req
+        return new Promise(function(resolve,reject){
+            let arr=[]
+            req.on('data',(buffer)=>{
+                arr.push(buffer)
+            })
+            req.on('end',()=>{
+                let reqData=JSON.parse(arr.concat().toString())
+                let admin=new Admin()
+                let result=admin.login(reqData)
+                resolve(result)
+            })
+        })
+    },
+    all(){
+        let admin=new Admin()
+        return admin.all()
+    },
+    add(req){
+        return new Promise((resolve,reject)=>{
+            let arr=[]
+            req.on('data',(buffer)=>{
+                arr.push(buffer)
+            })
+            req.on('end',()=>{
+                let reqData=JSON.parse(arr.concat().toString())
+                let admin=new Admin()
+                let result=admin.add(reqData)
+                resolve(result)
+            })
+        })
+    },
+    edit(){
+
+    },
+    delete(){
+
     }
 }
 
